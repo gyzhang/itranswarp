c8cd3751e28ee7e642c4255e96da4b3b1a9a0785
xprogrammer <xprogrammer@163.com>
2020-05-23 23:21:07 +0800

    使用ssl证书
    1. 修改本配置类；
    2. 修改application.yml，添加证书信息
    server:
      port: ${SERVER_PORT:443}
      ssl:
        enabled: true
        key-store: classpath:xxx.pfx
        key-store-password: YOUR PASSWORD HERE

--- src/main/java/com/itranswarp/ApplicationConfiguration.java
+++ src/main/java/com/itranswarp/ApplicationConfiguration.java
@@ -1,6 +1,13 @@
 package com.itranswarp;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -11,5 +18,37 @@ public class ApplicationConfiguration {
 
 	@Value("${spring.profiles.active:native}")
 	public String profiles;
+	
+	@Autowired
+	Connector httpConnector;
 
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpConnector);
+        return tomcat;
+    }
+ 
+    @Bean
+    public Connector httpConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        //Connector监听的http的端口号
+        connector.setPort(80);
+        connector.setSecure(false);
+        //监听到http的端口号后转向到的https的端口号
+        connector.setRedirectPort(443);
+        return connector;
+    }
+    
 }
